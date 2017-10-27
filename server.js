374d79f4c5065f441a70472461b6512fa36f1386
Thomas Angelo <thomasangelo@ThomasAngelo-MacBook-Pro.local>
2017-10-27 18:45:53 -0400

    update package and server files

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-app.use(
-    "/", //the URL throught which you want to access to you static content
-    express.static(__dirname) //where your static content is located in your filesystem
-);
-app.listen(3000); //the port you want to use
+var router = express.Router();
+var path = __dirname + '/views/';
+
+router.use(function (req,res,next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+  res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+  res.sendFile(path + "contact.html");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+  res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+  console.log("Live at Port 3000");
+});
