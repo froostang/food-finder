de14ec77aac9dfba497d13e6b7ddcf1e4f21110d
Thomas Angelo <thomasangelo@ThomasAngelo-MacBook-Pro.local>
2017-10-28 13:32:19 -0400

    updated 404

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
+var lib = __dirname + '/lib/';
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -20,10 +21,23 @@ router.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
 });
 
+// will server as api for testing
+router.get("/data",function(req,res){
+  var q = url.parse(req.url, true).query;
+  console.log(q);
+  // try {
+  //   var txt = q.user + " is your user request";
+  // } catch(err) {
+  //   var txt = "incorrect query string, try 'user'";
+  // }
+  //res.end(txt);
+});
+
 app.use("/",router);
 
 app.use("*",function(req,res){
   res.sendFile(path + "404.html");
+  res.send('404 Error: Invalid Path.', 404);
 });
 
 app.listen(3000,function(){
