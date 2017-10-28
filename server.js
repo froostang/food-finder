a06e9297156c1a9e295ede81c29a5f6cc94acc1e
Alec Swanson <alecswanson@Alecs-MacBook-Pro.local>
2017-10-27 20:05:20 -0400

    added a few things

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ app.use(
     "/", //the URL throught which you want to access to you static content
     express.static(__dirname) //where your static content is located in your filesystem
 );
-app.listen(3000); //the port you want to use
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+}); //the port you want to use
+
