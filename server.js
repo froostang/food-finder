4db30e399271180bf3613a155bc940cd1d1105a5
Thomas Angelo <thomasangelo@ThomasAngelo-MacBook-Pro.local>
2017-10-31 01:13:10 -0400

    update routing and response of /data

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 var lib = __dirname + '/lib/';
+var bodyParser = require('body-parser');
+var yt = require('./lib/yelp-testing.js');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -23,14 +30,26 @@ router.get("/contact",function(req,res){
 
 // will server as api for testing
 router.get("/data",function(req,res){
-  var q = url.parse(req.url, true).query;
-  console.log(q);
-  // try {
-  //   var txt = q.user + " is your user request";
-  // } catch(err) {
-  //   var txt = "incorrect query string, try 'user'";
-  // }
-  //res.end(txt);
+  var lat = req.query.lat;
+  var long = req.query.long;
+
+  // just for testing display, remove when correct JSON returned from getYelpFood
+  var ob = {
+      "lat": lat,
+      "long": long
+  }
+  // everything works but I -think-
+  // I need a callback based on the value returned from getYelpFood
+  // or a callback in yelp-testing function to return data
+
+  //var final = yt.getYelpFood(lat, long);
+  //res.json(ob);
+  if(lat == undefined || long == undefined) {
+    res.send("Please specify query string with lat and long ex. /data?lat=100&long=100");
+  } else {
+    res.json(ob);  
+  }
+
 });
 
 app.use("/",router);
