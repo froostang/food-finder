eb68b48d480d449836fcbea9146cb8ab35b7f581
Thomas Angelo <thomasangelo@ThomasAngelo-MacBook-Pro.local>
2017-10-31 21:52:14 -0400

    api fixes for /data and returning json

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var router = express.Router();
 var path = __dirname + '/views/';
 var lib = __dirname + '/lib/';
 var bodyParser = require('body-parser');
+// contins function for returning yelp JSON based on lat/long
 var yt = require('./lib/yelp-testing.js');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -28,26 +30,18 @@ router.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
 });
 
-// will server as api for testing
+// pass query string with lat/long and queries yelp API
 router.get("/data",function(req,res){
   var lat = req.query.lat;
   var long = req.query.long;
 
-  // just for testing display, remove when correct JSON returned from getYelpFood
-  var ob = {
-      "lat": lat,
-      "long": long
-  }
-  // everything works but I -think-
-  // I need a callback based on the value returned from getYelpFood
-  // or a callback in yelp-testing function to return data
-
-  //var final = yt.getYelpFood(lat, long);
-  //res.json(ob);
   if(lat == undefined || long == undefined) {
+    // default return if querie(s) don't exist
     res.send("Please specify query string with lat and long ex. /data?lat=100&long=100");
   } else {
-    res.json(ob);  
+    yt.getYelpFood(lat, long).then(function(data) {
+      res.json(data);
+    });
   }
 
 });
